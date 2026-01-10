0191b1c27da9ddf3f1e7c2a43da54033b40cc8d3
gagansv <gagansv8@gmail.com>
2026-01-10 10:06:05 +0530

    frontend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AdminDashboard from "./components/AdminDashboard";
+import ProductsPage from "./pages/ProductsPage";
+import VerifyPurchase from "./pages/VerifyPurchase";
+import ReviewPage from "./pages/ReviewPage";
 
 export default function App() {
   return (
@@ -8,6 +11,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/verify" element={<VerifyPurchase />} />
+        <Route path="/review" element={<ReviewPage />} />
       </Routes>
     </BrowserRouter>
   );
